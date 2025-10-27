5f06e912d04b48ff9ebbbbf356ff6fabc930e4b5
kmoy22 <kevinmmoy0@gmail.com>
2025-10-27 08:30:13 +0000

    Wrap up

--- tests/regression/dot8/assignment5.sh
+++ tests/regression/dot8/assignment5.sh
@@ -2,15 +2,17 @@
 
 start_time=$SECONDS
 
-TEST="--app=dot8 --args="-n256""
 DEBUG="--debug=1"
 # Debug 1 for lower verbosity, Debug 3 for highest verbosity
 
-./ci/blackbox.sh --driver=simx --cores=4 --warps=4 --threads=4 $TEST | tee "simx_4w4t.log"
-./ci/blackbox.sh --driver=simx --cores=4 --warps=4 --threads=8 $TEST | tee "simx_4w8t.log"
-./ci/blackbox.sh --driver=simx --cores=4 --warps=8 --threads=4 $TEST | tee "simx_8w4t.log"
-./ci/blackbox.sh --driver=simx --cores=4 --warps=8 --threads=8 $TEST | tee "simx_8w8t.log"
-
+./ci/blackbox.sh --driver=simx --cores=4 --warps=4 --threads=4 --app=dot8 --args="-n256" | tee "simx_4w4t_dot8.log"
+./ci/blackbox.sh --driver=simx --cores=4 --warps=4 --threads=8 --app=dot8 --args="-n256" | tee "simx_4w8t_dot8.log"
+./ci/blackbox.sh --driver=simx --cores=4 --warps=8 --threads=4 --app=dot8 --args="-n256" | tee "simx_8w4t_dot8.log"
+./ci/blackbox.sh --driver=simx --cores=4 --warps=8 --threads=8 --app=dot8 --args="-n256" | tee "simx_8w8t_dot8.log"
+./ci/blackbox.sh --driver=simx --cores=4 --warps=4 --threads=4 --app=sgemm --args="-n256" | tee "simx_4w4t_sgemm.log"
+./ci/blackbox.sh --driver=simx --cores=4 --warps=4 --threads=8 --app=sgemm --args="-n256" | tee "simx_4w8t_sgemm.log"
+./ci/blackbox.sh --driver=simx --cores=4 --warps=8 --threads=4 --app=sgemm --args="-n256" | tee "simx_8w4t_sgemm.log"
+./ci/blackbox.sh --driver=simx --cores=4 --warps=8 --threads=8 --app=sgemm --args="-n256" | tee "simx_8w8t_sgemm.log"
 end_time=$SECONDS
 
 elapsed_time=$((end_time - start_time))
--- tests/regression/dot8/assignment6.sh
+++ tests/regression/dot8/assignment6.sh
@@ -2,14 +2,17 @@
 
 start_time=$SECONDS
 
-TEST="--app=dot8 --args="-n256""
 DEBUG="--debug=1"
 # Debug 1 for lower verbosity, Debug 3 for highest verbosity
 
-./ci/blackbox.sh --driver=rtlsim --cores=4 --warps=4 --threads=4 --app=dot8 --args="-n256" | tee "rtl_4w4t.log"
-./ci/blackbox.sh --driver=rtlsim --cores=4 --warps=4 --threads=8 --app=dot8 --args="-n256" | tee "rtl_4w8t.log"
-./ci/blackbox.sh --driver=rtlsim --cores=4 --warps=8 --threads=4 --app=dot8 --args="-n256" | tee "rtl_8w4t.log"
-./ci/blackbox.sh --driver=rtlsim --cores=4 --warps=8 --threads=8 --app=dot8 --args="-n256" | tee "rtl_8w8t.log"
+./ci/blackbox.sh --driver=rtlsim --cores=4 --warps=4 --threads=4 --app=dot8 --args="-n256" | tee "rtl_4w4t_dot8.log"
+./ci/blackbox.sh --driver=rtlsim --cores=4 --warps=4 --threads=8 --app=dot8 --args="-n256" | tee "rtl_4w8t_dot8.log"
+./ci/blackbox.sh --driver=rtlsim --cores=4 --warps=8 --threads=4 --app=dot8 --args="-n256" | tee "rtl_8w4t_dot8.log"
+./ci/blackbox.sh --driver=rtlsim --cores=4 --warps=8 --threads=8 --app=dot8 --args="-n256" | tee "rtl_8w8t_dot8.log"
+./ci/blackbox.sh --driver=rtlsim --cores=4 --warps=4 --threads=4 --app=sgemm --args="-n256" | tee "rtl_4w4t_sgemm.log"
+./ci/blackbox.sh --driver=rtlsim --cores=4 --warps=4 --threads=8 --app=sgemm --args="-n256" | tee "rtl_4w8t_sgemm.log"
+./ci/blackbox.sh --driver=rtlsim --cores=4 --warps=8 --threads=4 --app=sgemm --args="-n256" | tee "rtl_8w4t_sgemm.log"
+./ci/blackbox.sh --driver=rtlsim --cores=4 --warps=8 --threads=8 --app=sgemm --args="-n256" | tee "rtl_8w8t_sgemm.log"
 
 end_time=$SECONDS
 
